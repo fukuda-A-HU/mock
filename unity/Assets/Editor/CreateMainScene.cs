135715daff55b7d85b615035010187300f7d782d
Eiki Fukuda <e-fukuda@EikinoMacBook-Neo.local>
2026-06-20 16:43:26 +0900

    checkpoint before checking out main

--- unity/Assets/Editor/CreateMainScene.cs
+++ unity/Assets/Editor/CreateMainScene.cs
@@ -1,16 +1,18 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public static class CreateMainScene
 {
     public static void Create()
     {
         var scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
-        var controller = new GameObject("MainScene");
-        controller.AddComponent<MainSceneController>();
-        EditorSceneManager.SaveScene(scene, "Assets/Scenes/MainScene.unity");
-        EditorBuildSettings.scenes = new[] { new EditorBuildSettingsScene("Assets/Scenes/MainScene.unity", true) };
+        var controller = new GameObject("Quest Guild Game");
+        controller.AddComponent<UIDocument>();
+        controller.AddComponent<QuestGuildGame>();
+        EditorSceneManager.SaveScene(scene, "Assets/Scenes/Main.unity");
+        EditorBuildSettings.scenes = new[] { new EditorBuildSettingsScene("Assets/Scenes/Main.unity", true) };
         AssetDatabase.SaveAssets();
         EditorApplication.Exit(0);
     }
